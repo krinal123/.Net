be8696e49480e5c50f8c72c2e4df310496079416
“mirajsankdecha” <mirajsankdecha@gmail.com>
2023-06-29 21:32:29 +0530

    Three Basic Program

---
+++ Unit-1/Unit-1/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_1
+{
+    internal class Extension
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the File Name: ");
+            String s = Console.ReadLine();
+            string result = s.Substring(s.LastIndexOf('.') + 1);
+            Console.WriteLine("Your Extension is : " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unit-1/Unit-1/Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_1
+{
+    internal class Sum
+    { 
+        public static Main(string[] args)
+        {
+            Console.Write("Enter the Value of A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Value of B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            Console.Write("Sum of Two Numbers is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
