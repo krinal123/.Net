45df8f5388bbb3abd64cdc8b47494c80cacbf96f
“mirajsankdecha” <mirajsankdecha@gmail.com>
2023-07-03 13:13:27 +0530

    Reverse string in word logic update

--- Unit-1/Unit-1/Revword_sec.cs
+++ Unit-1/Unit-1/Revword_sec.cs
@@ -12,14 +12,11 @@ namespace Unit_1
         {
             Console.Write("Enter the String : "); 
             string str = Console.ReadLine();
-            string[] s =str.Split(' ');
-            string ans = "";
-            for (int i = s.Length - 1;i >= 0; i--)
-            {
-                ans += s[i] + " ";
-            }
-            Console.Write("Reversed String: ");
-            Console.Write(ans.Substring(0,ans.Length - 1)); 
+            
+            string ans = string.Join(" ",str.Split(' ').Reverse());
+            
+            Console.Write("Reversed String: " + ans);
+             
             Console.WriteLine();
 
         }
