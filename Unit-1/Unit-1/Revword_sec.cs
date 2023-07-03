4676d710c5861b701d1b58cadb00ea084a8a0cc8
“mirajsankdecha” <mirajsankdecha@gmail.com>
2023-07-03 13:03:49 +0530

    Reverse string in word

---
+++ Unit-1/Unit-1/Revword_sec.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_1
+{
+    internal class Revword_sec
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the String : "); 
+            string str = Console.ReadLine();
+            string[] s =str.Split(' ');
+            string ans = "";
+            for (int i = s.Length - 1;i >= 0; i--)
+            {
+                ans += s[i] + " ";
+            }
+            Console.Write("Reversed String: ");
+            Console.Write(ans.Substring(0,ans.Length - 1)); 
+            Console.WriteLine();
+
+        }
+    }
+}
